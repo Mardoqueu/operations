b85243bd40d2d69c7d8d363b477c1dab7c4f0a19
mardoqueu.telvina <mardoqueu.telvina@cibernix.com>
2024-11-11 20:38:41 -0300

    feat: Add support for negative numbers and handle sqrt of negative values

--- src/main/java/com/challenge/operations/exception/InvalidOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.challenge.operations.exception;
-
-public class InvalidOperationException extends RuntimeException {
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/challenge/operations/exception/InvalidSquareRootException.java
+++
@@ -1,7 +0,0 @@
-package com.challenge.operations.exception;
-
-public class InvalidSquareRootException extends RuntimeException {
-    public InvalidSquareRootException(String message) {
-        super(message);
-    }
-}
